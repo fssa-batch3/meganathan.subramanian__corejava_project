4762456f402d208a5f560eea70618deec692056d
Meganathan <meganathan212003@gmail.com>
2023-07-13 17:38:35 +0530

    added day06 practice java files

--- src/main/java/day06/practice/StoreTask.java
+++ src/main/java/day06/practice/StoreTask.java
@@ -1,9 +1,33 @@
 package day06.practice;
 
+import java.util.ArrayList;
+
 public class StoreTask {
-	
+
+	public String taskName;
+	public int dueDate;
+
+	// Below the code for constructor
+
+	public StoreTask(String taskName, int dueDate) {
+		this.taskName = taskName;
+		this.dueDate = dueDate;
+	}
+
 	public static void main(String[] args) {
+
+		// Creating a new array list name called task
+		ArrayList<StoreTask> task = new ArrayList<>();
+
+		// Add task to the ArrayList
+		task.add(new StoreTask("Dinesh", 12));
+		task.add(new StoreTask("Aravind", 10));
 		
+		// Print all the task 
+		for()
+			
+
 	}
 
 }
+   
