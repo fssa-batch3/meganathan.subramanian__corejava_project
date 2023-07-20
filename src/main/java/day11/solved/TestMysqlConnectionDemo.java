11d811104ef9c1c458299299a0032bf2b0eff5c6
Meganathan <meganathan212003@gmail.com>
2023-07-20 13:48:38 +0530

    added mysql connection file

---
+++ src/main/java/day11/solved/TestMysqlConnectionDemo.java
@@ -0,0 +1,29 @@
+package day11.solved;
+
+import java.sql.*;
+
+public class TestMysqlConnectionDemo {
+
+	public static void main(String[] args) throws SQLException {
+
+		String url = "jdbc:mysql://localhost:3306/fssa";
+		String userName = "root";
+		String password = "root";
+
+		String query = "SELECT * FROM users";
+
+		Connection con = null;
+		try
+		{
+			con = DriverManager.getConnection(url, userName, password);
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+
+		System.out.println("connection created...");
+
+		con.close();
+	}
+}
